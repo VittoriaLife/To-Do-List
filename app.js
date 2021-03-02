3dfde741722883e21ad5f35fd92e59ca6a39b7f9
Viktoria <vitagood.92@list.ru>
2021-03-02 17:11:49 +0300

    new

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
 'use strict';
 
 
-
-
-function getInfo() {
-  fetch('https://run.mocky.io/v3/c77c21cc-3fe1-4496-ae53-92f018b6248a').then(response => response.json()).then(toDoList => {
-    const list = document.querySelector('.todos');
-
-    toDoList.forEach(element => {
-      const li = document.createElement('li');
-      const span = document.createElement('span');
-      const button = document.createElement('button');
-
-      span.textContent = element.description;
-      button.textContent = 'Удалить';
-
-      li.append(span);
-      li.append(button);
-      list.append(li);
-
-      button.addEventListener('click', () => {
-        deleteToDoListElement(element._id)
-          .then(response => response.json())
-          .then(data => {
-            list.textContent = null;
-            getInfo();
-          });
-      })
-    });
-  });
-};
-
-getInfo();
