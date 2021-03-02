3922df113026a9aa4805fca945d6c4f515d40095
Victoria <vitagood.92@list.ru>
2021-03-02 13:37:13 +0300

    1

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 'use strict';
 
-let localItems = window.localStorage;
+
+
 
 function getInfo() {
-  fetch('').then(response => response.json()).then(list => console.log(list));
+  fetch('https://run.mocky.io/v3/c77c21cc-3fe1-4496-ae53-92f018b6248a').then(response => response.json()).then(toDoList => {
+    const list = document.querySelector('.todos');
+
+    toDoList.forEach(element => {
+      const li = document.createElement('li');
+      const span = document.createElement('span');
+      const button = document.createElement('button');
+
+      span.textContent = element.description;
+      button.textContent = 'Удалить';
+
+      li.append(span);
+      li.append(button);
+      list.append(li);
+
+      button.addEventListener('click', () => {
+        deleteToDoListElement(element._id)
+          .then(response => response.json())
+          .then(data => {
+            list.textContent = null;
+            getInfo();
+          });
+      })
+    });
+  });
 };
 
 getInfo();
