ff5d0878c7bd0be0b24e0e794d2a8d4c3c145b51
Victoria <vitagood.92@list.ru>
2021-03-03 01:37:21 +0300

    app

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 'use strict';
 
+const list = document.querySelector('.list');
+const listItem = document.querySelector('.list__item');
+const formInput = document.querySelector('.form__input');
+const formBtn = document.querySelector('.form__btn');
 
+
+function addListItem() {
+  formBtn.addEventListener('change', function() {
+    const li = document.createElement('li');
+    li.classList.add('list__item');
+    li.innerText = formBtn.value;
+    list.appendChild(li);
+  })
+
+  return li;
+};
+
+
+
+formBtn.addEventListener('click', function() {
+  addListItem();
+
+  // list.append(li);
+})
