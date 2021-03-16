73674fe6854dfd1a29da3c44425b98737faa58bf
Victoria <vitagood.92@list.ru>
2021-03-16 22:40:47 +0300

    my

--- js/app.js
+++ js/app.js
@@ -1,105 +1,147 @@
 'use strict';
 
-// const clear = // clean btn
-//const date
-
-const list = document.querySelector('.list');
+const list = document.querySelector('.list')
 const formInput = document.querySelector('.form__input');
-let LIST = [];
-let id = 0;
+const formBtn = document.querySelector('.form__btn');
+const itemsIdArray = [];
+const buttonsIdArray = []
 
 
+function addListItem() {
+  const li = document.createElement('li');
+  const p = document.createElement('p');
+  const btn = document.createElement('button');
+  let toDoValue = formInput.value;
+
+  li.classList.add('list__item');
+  p.classList.add('list__text');
+  btn.classList.add('list__btn-delete');
+
+  if(toDoValue) {
+    p.innerText = formInput.value;
+    li.append(p);
+    li.append(btn);
+    list.append(li);
+    formInput.value = ' ';
+    formInput.setCustomValidity('');
+  } else {
+    formInput.setCustomValidity('Please, enter to-do');
+  }
+
+};
 
 
-function addToDo (toDo) {
-  const text = `<li class="list__item">
-                <p class="list__text">${toDo}</p>
-              </li>`
-              
-  const position = "beforeend";
+function setId() {
+  let liItems = document.querySelectorAll('.list__item');
+  let deleteButtons = document.querySelectorAll('.list__btn-delete');
 
-  list.insertAdjacentHTML(position, text);
+  for(let i = 0; i < liItems.length; i++) {
+    liItems[i].setAttribute('id', i);
+    deleteButtons[i].setAttribute('id', i);
+  }
+
+  for(let k = 0; k < liItems.length; k++){
+    let item = liItems[k].getAttribute('id');
+    let button = deleteButtons[k].getAttribute('id');
+    
+    itemsIdArray[k] = item;
+    buttonsIdArray[k] = button;
+  };
 };
 
 
 
-document.addEventListener('keyup', function(evt) {
-  if(evt.keyCode === 13){
-    const toDo = formInput.value;
-      if(toDo) {
-        addToDo(toDo, id, false, false);
 
-        LIST.push( 
-          {
-            name: toDo,
-            id: id,
-            done: false,
-            trash: false
-          }
-        );
-        formInput.value = '';
-        id++;
-      }
-     
-  }
+formBtn.addEventListener('click', function(evt) {
+  // evt.preventDefault();
+
+  addListItem();
+  // setId();
+ 
 });
 
 
 
-// const formBtn = document.querySelector('.form__btn');
-// const itemsIdArray = [];
-// const buttonsIdArray = []
+
+// const clear = document.querySelector('.clear');
+// const dateElement = document.getElementById('date');
+// const list = document.getElementById('list');
+// const input = document.getElementById('input');
+// const CHECK = 'fa-check-cirkle';
+// const UNCHEK = 'fa-circle-thin';
+// const LINE__THROUGH = 'lineThrough';
+
+// let LIST = [];
+// let id = 0;
 
 
-// function addListItem() {
-//   const li = document.createElement('li');
-//   const p = document.createElement('p');
-//   const btn = document.createElement('button');
 
-//   li.classList.add('list__item');
-//   p.classList.add('list__text');
-//   btn.classList.add('list__btn-delete');
 
-//   if(formInput) {
-//     p.innerText = formInput.value;
-//     li.append(p);
-//     li.append(btn);
-//     list.append(li);
-//     formInput.value = ' ';
+// function addToDo (toDo, id, done, trash) {
+
+//   if(trash) {
+//     return;
 //   }
 
+//   const DONE = done ? CHECK : UNCHEK;
+//   const LINE  = done ? LINE__THROUGH : '';
+
+//   const text = `<li class="item">
+//                   <i class="fa ${DONE} complete" job="complite id="${id}"></i>
+//                   <p class="text ${LINE}">${toDo}</p>
+//                   <i class="fa fa-trash-o delete" job="delete" id="${id}"></i>
+//               </li>`
+              
+//   const position = 'beforeend';
+
+//   list.insertAdjacentHTML(position, text);
 // };
 
 
+// function completeToDO(element) {
+//   element.classList.toggle(CHECK);
+//   element.classList.toggle(UNCHEK);
 
-
-// function setId() {
-//   let liItems = document.querySelectorAll('.list__item');
-//   let deleteButtons = document.querySelectorAll('.list__btn-delete');
-
-//   for(let i = 0; i < liItems.length; i++) {
-//     liItems[i].setAttribute('id', i);
-//     deleteButtons[i].setAttribute('id', i);
-//   }
-
-//   for(let k = 0; k < liItems.length; k++){
-//     let item = liItems[k].getAttribute('id');
-//     let button = deleteButtons[k].getAttribute('id');
-    
-//     itemsIdArray[k] = item;
-//     buttonsIdArray[k] = button;
-//   };
+//   element.parentNode.querySelector('.text').classList.toggle(LINE__THROUGH);
+//   LIST[element.id].done = LIST[element.id].done ? false : true;
 // };
 
+// function removeToDo(el) {
+//   el.parentNode.parentNode.removeChild(el.parentNode);
+
+//   LIST[el.id].trash = true;
+// }
 
 
+// list.addEventListener('click', function name(evt) {
+//   let element = evt.target;
 
-// formBtn.addEventListener('click', function(evt) {
-//   evt.preventDefault();
+//   const elementJob = evt.target.attributes.job.value;
 
-//   addListItem();
-//   // setId();
- 
+//   if(elementJob === 'complete') {
+//       completeToDO(element);
+//   } else if(elementJob === 'delete') {
+//       removeToDo(element);
+//   }
 // });
 
 
+// document.addEventListener('keyup', function(evt) {
+//   if(evt.keyCode === 13){
+//     const toDo = input.value;
+//       if(toDo) {
+//         addToDo(toDo, id, false, false);
+
+//         LIST.push( 
+//           {
+//             name: toDo,
+//             id: id,
+//             done: false,
+//             trash: false
+//           }
+//         );
+//         input.value = '';
+//         id++;
+//       }
+//   }
+// });
