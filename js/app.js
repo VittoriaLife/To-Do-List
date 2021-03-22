1b0c35b9ec07d4aa9d32574770b9c748d61e0719
Viktoria <vitagood.92@list.ru>
2021-03-22 16:21:17 +0300

    1

--- js/app.js
+++ js/app.js
@@ -2,63 +2,100 @@
 
 import {getData, recordData, deleteToDoItem} from './database.js';
 
-const formInput = document.querySelector('.form__input');
+//
+const formInput = document.querySelector('.form__input'); 
+
+//
 const formBtn = document.querySelector('.form__btn');
 
+
+//
 function init(params) {
+
+  //
   const dataStore = getData('todo');
 
+  //
   if(!dataStore) {
     return;
   }
 
+  //
   dataStore.forEach((toDo) => {
     createToDo(toDo.id, toDo.text);
   })
 }
 
+
+//
 init();
 
+//
+function deleteToDo(id) {
+
+  //
+  deleteToDoItem(id, 'todo');
+
+  //
+  const list = document.querySelector('.list');
+  //
+  const listItem = list.querySelector(`[toDoId='${id}']`);
+
+  //
+  listItem.remove();
+};
+
+//
 function createToDo(id, text) {
+
+  //
   const list = document.querySelector('.list');
 
+  //
   const listItem = document.createElement('li');
   listItem.classList.add('list__item');
   listItem.setAttribute('toDoId', id);
   
+  //
   const listText = document.createElement('p');
   listText.textContent = text;
 
+  //
   const listButton = document.createElement('button');
   listButton.classList.add('list__btn-delete');
   listButton.setAttribute('toDoId', id);
 
+  //
   listButton.addEventListener('click', (evt) => {
+    //
     const getId = evt.target.getAttribute('toDoId');
+
+    //
     deleteToDo(getId);
   });
 
+  //
   listItem.append(listText);
+  
+  //
   listItem.append(listButton);
-
+ 
+  //
   list.append(listItem);
 };
 
-function deleteToDo(id) {
-  deleteToDoItem(id, 'todo');
-
-  const list = document.querySelector('.list');
-  const listItem = list.querySelector(`[toDoId='${id}']`);
-
-  listItem.remove();
-};
-
+//
 formBtn.addEventListener('click', function(evt) {
+  //
   evt.preventDefault();
 
+  //
   const id = recordData(formInput.value, 'todo');
+
+  //
   createToDo(id, formInput.value);
   
+  //
   formInput.value = '';
 });
 
--- js/database.js
+++ js/database.js
@@ -1,35 +1,54 @@
+'use strict';
+
+// функция получения данных из localStorage, принимает в себя ключ(имя)
 export function getData(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
-
+// функция создания данных для localStorage
 export function recordData(text, key) {
-  const id = (+new Date()).toString(16);
 
-  const newToDo = {
+  // создание id путем генерации числа в миллисекундах из даты, превращения этого числа в строку с использованием 16 системы исчисления
+  const id = (+new Date()).toString(16); 
+
+  // новый пункт списка в виде объекта
+  const newToDo = { 
     text,
-    id,
-    done: false
+    id
   }
 
+  // получаем в переменную данные из LocalStorage
   let list = getData(key);
 
+  //если переменная list пуста(false), то list становится пустым массивом
   if(!list) {
     list = [];
   }
+
+  // записываем в пустой массив list новые пункты списка в виде объектов
    list.push(newToDo);
 
+
+   // записываем в LocalStorage имя ключа(key), массив объектов (значение ключа(key)) list в виде строки.
   localStorage.setItem(key, JSON.stringify(list));
 
+
+  // возвращаем id для дальнейшщего использования
   return id;
 }
 
+// функция удаления пункта списка
 export function deleteToDoItem(id, key) {
+
+  // получаем в переменную данные из LocalStorage
   const allItems = getData(key);
 
-  const newList = allItems.filter((todo) => {
+  // создаем новый массив элементов, оставляя в нем только те элементы чьи id не совпадают(false)
+  const newList = allItems.filter(todo => {
     return todo.id !== id;
   });
 
+
+  // записываем в localStorage новый массив элементов
   localStorage.setItem(key, JSON.stringify(newList));
 }
